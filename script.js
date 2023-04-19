c444b887a800ac43cc1b43381eea8922dcf994a0
Moe <2395582@students.wits.ac.za>
2023-04-19 13:52:33 +0200

    b*tch im stylish
    black top, big tshirt b...

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-function myFunction() {
+function firstCase() {
     var element = document.body;
     element.classList.toggle("modern");
+   
   }
+
+  function colorOne(){
+    var element = document.body;
+    element.classList.toggle("colorone");
+  }
+
+  function colorTwo(){
+    var element = document.body;
+    element.classList.toggle("colortwo");
+  }
+
+  function colorThree(){
+    var element = document.body;
+    element.classList.toggle("colorthree");
+  }
+
+  function fontOne(){
+    var element = document.body;
+    element.classList.toggle("fontone");
+  }
+
+  function fontTwo(){
+    var element = document.body;
+    element.classList.toggle("fonttwo");
+  }
+
+  function fontThree(){
+    var element = document.body;
+    element.classList.toggle("fontthree");
+  }
+
